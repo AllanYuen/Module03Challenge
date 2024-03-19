908c8f890a259b54675bbfe6390eaf507604a09c
Allan Yuen <145529886+AllanYuen@users.noreply.github.com>
2024-03-19 16:35:50 -0400

    Update PR to include cloned starter code

---
+++ README.md
@@ -0,0 +1,46 @@
+# Module03Challenge
+
+JavaScript Challenge: Password Generator
+
+## Description
+
+This JavaScript application allows employees to generate random passwords based on their selected criteria. It provides a user-friendly interface where employees can specify the length of the password and choose from various options such as including uppercase letters, lowercase letters, numbers, and special characters.
+
+The motivation behind building this project was to provide a convenient tool for employees to generate strong and secure passwords that meet their specific requirements. By using this application, employees can easily create unique passwords that help protect their accounts and sensitive information.
+
+This application solves the problem of coming up with strong passwords that are difficult to guess or crack. It eliminates the need for employees to manually think of complex passwords and provides a reliable way to generate random passwords that meet the desired criteria.
+
+Through building this project, I learned about JavaScript programming concepts such as generating random numbers, manipulating strings, and working with user input.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+Please refer to the [LICENSE](./LICENSE) file for more details.
+
+## Features
+
+If your project has a lot of features, list them here.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,115 +1,17 @@
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
+// Assignment code here
 
-// Declaration of required variables to support the password generator
-var pwdLength = null;
-var UCLength = null;
-var LCLength = null;
-var numLength = null;
-var symbolLength = null;
-var lengthRemain = null;
-var pwdCriteria = {
-  numericChar: "0123456789".split(""),
-  upperCaseChar: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
-  lowerCaseChar: "abcdefghijklmnopqrstuvwxyz".split(""),
-  specialChar: "!#$%&()*+,-./:;<=>?@[\]^_`{|}~".split("")
-};
 
-// Function for user input to indicate the required password length
-function passwordLength() {
-  var overallLength = prompt("Please indicate your required password length (between 8 to 128 bytes in length)","Enter a numeric value from 8 to 128");
-  var pwdLength = Number(overallLength);
-  if (pwdLength >= 8 && pwdLength <= 128) {
-    alert("You've entered: " + pwdLength);
-    lengthRemain = pwdLength;
-  }
-    else {
-      alert("The length value entered is not within the range of 1 - 128.");
-    }
-    return;
-  }
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 
-// Prompt for user input to indicate the number of UPPERCASE characters required in password
-function upperCaseLength() {
-  var upperLength = prompt("Please indicate your minimum number of required UPPERCASE characters","Enter a number between 0 and " + remainingLength);
-  var UCLength = Number(upperLength);
-  if (UCLength <= lengthRemain) {
-    alert("You've entered: " + UCLength);
-    lengthRemain = lengthRemain - UCLength;
-  }
-    else {
-      alert("The length value exceeds the remaining password length availability.");
-    }
-    return;
-  }
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// Prompt for user input to indicate the number of lowercase characters required in password
-function lowerCaseLength() {
-  var lowerLength = prompt("Please indicate your minimum number of required lowercase characters","Enter a number between 0 and " + remainingLength);
-  var LCLength = Number(lowerLength);
-  if (LCLength <= lengthRemain) {
-    alert("You've entered: " + LCLength);
-    lengthRemain = lengthRemain - LCLength;
-  }
-    else {
-      alert("The length value exceeds the remaining password length availability.");
-    }
-    return;
-  }
+  passwordText.value = password;
 
-// Prompt for user input to indicate the number of numeric characters required in password
-function numericLength() {
-  var numberLength = prompt("Please indicate your minimum number of required numeric characters","Enter a number between 0 and " + remainingLength);
-  var numLength = Number(numberLength);
-  if (numLength <= lengthRemain) {
-    alert("You've entered: " + numLength);
-    lengthRemain = lengthRemain - numLength;
-  }
-    else {
-      alert("The length value exceeds the remaining password length availability.");
-    }
-    return;
-  }
-
-// Prompt for user input to indicate the number of special characters required in password
-function specialCharLength() {
-  var specCharLength = prompt("Please indicate your minimum number of required special characters","Enter a number between 0 and " + remainingLength);
-  var symbolLength = Number(specCharLength);
-  if (symbolLength <= lengthRemain) {
-    alert("You've entered: " + symbolLength);
-    lengthRemain = lengthRemain - symbolLength;
-  }
-    else {
-      alert("The length value exceeds the remaining password length availability.");
-    }
-    return;
-  }
-
-// Prompt for user input to provide password criteria to use in generating a new password
-function userPwdCriteriaPrompt() {
-  if (pwdLength === null) {
-    passwordLength();
-    if(UCLength === null) {
-      upperCaseLength()
-    }
-  }
-  else {
-    alert("The current password length is set to: "+ lengthRemain);
-  } 
-  return;
 }
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-// userPwdCriteriaPrompt();
-
-userPwdCriteriaPrompt();
+generateBtn.addEventListener("click", writePassword);
